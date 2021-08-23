75e56932f9f6f6c2dfaafa9e7182d60c0b0036ec
iminjae <iminjae@172.30.1.7>
2021-08-23 20:32:07 +0900

    23일 이민재 두번째

--- src/com/kh/team3/chat/controller/ChatServlet.java
+++ src/com/kh/team3/chat/controller/ChatServlet.java
@@ -28,7 +28,7 @@ public class ChatServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher view =request.getRequestDispatcher("views/market/chatroom.jsp");
+		RequestDispatcher view =request.getRequestDispatcher("views/chat/chatroom.jsp");
 	    view.forward(request, response);
 	}
 
